34d4947dc8e4f3287b847f38c61602c1c34f1e50
RUFF <guiguiruff@gmail.com>
2022-06-03 17:57:36 +0200

    Bulding the request for the ContactInfo Form

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import {ContactInfo} from "../Pages/a-propos/ContactInfo";
@@ -14,8 +14,13 @@ export class ApiService {
 
 
   sendAboutContactForm(contactForm : ContactInfo ){
-    const url = "community/contact/create/" //TODO DEFINE A GLOBAL VARIABLE FOR URL PATH
-     return this.http.post<any>(url, contactForm.name,, httpOptions)
+    const url = 'community/contact/create/'; //TODO DEFINE A GLOBAL VARIABLE FOR URL PATH and add it via
+    const httpOptions = {
+      headers: new HttpHeaders({})
+    };
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+     return this.http.post<any>(url,{name:contactForm.name, last_name:contactForm.lastName,
+       email:contactForm.email, phone:contactForm.phone, subject:contactForm.subject, message:contactForm.message},httpOptions)
        .pipe(
          catchError(this.handleError('addHero', hero))
        );
