8ac60beb83d0452e5acfe44558f92e96db8e5bae
Guillaume <guiguiruff@gmail.com>
2022-06-18 16:49:23 +0200

    Build the Ui element for the profile page, need some tweak to the css and the methods ask the back end

--- src/app/Pages/profile/profile.module.ts
+++ src/app/Pages/profile/profile.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { ProfilePageRoutingModule } from './profile-routing.module';
 
 import { ProfilePage } from './profile.page';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ProfilePageRoutingModule
+    ProfilePageRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatCheckboxModule
   ],
   declarations: [ProfilePage]
 })
--- src/app/Pages/profile/profile.page.ts
+++ src/app/Pages/profile/profile.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
+import {ApiService} from '../../services/api.service';
+import {CdkTextareaAutosize} from '@angular/cdk/text-field';
+import {take} from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +11,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  @ViewChild('autosizeDiscipline') autosizeDiscipline: CdkTextareaAutosize;
+  @ViewChild('autosizeRecherche') autosizeRecherche: CdkTextareaAutosize;
 
-  ngOnInit() {
+  selectedTab = 'information-personnelles';
+  ipFrom: FormGroup;
+  disciplineFrom: FormGroup;
+  rechercheFrom: FormGroup;
+  establishmentFrom: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private ngZone: NgZone, //used to resized text area
+    private api: ApiService
+  ) { }
+
+  //TODO Check ou le trigger
+  triggerResize() {
+    // Wait for changes to be applied, then trigger textarea resize.
+    this.ngZone.onStable.pipe(take(1)).subscribe(() => this.autosizeDiscipline.resizeToFitContent(true));
+    this.ngZone.onStable.pipe(take(1)).subscribe(() => this.autosizeRecherche.resizeToFitContent(true));
   }
 
+  ngOnInit() {
+    this.ipFrom = this.fb.group({
+      name: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required], Validators.email],
+      address: ['', [Validators.required, Validators.pattern('^[0-9]+$')]], //Accept only number
+      town: ['', [Validators.required]],
+      postalCode: ['', [Validators.required]],
+      workInCompagnie: [''],
+      compagnie:[''],
+      compagnieTime:['']
+  });
+    this.disciplineFrom = this.fb.group({
+      disciplineName:[''],
+      disciplineText: ['']
+    });
+
+    this.rechercheFrom = this.fb.group({
+      rechercheName:[''],
+      rechercheText: ['']
+    });
+
+    this.establishmentFrom = this.fb.group({
+      establishmentName:[''],
+      labName: [''],
+      establishmentText: ['']
+    });
+
+
+  }
+
+  ipOnSubmit(){
+
+  }
+
+  disciplineOnSubmit(){
+
+  }
+
+  rechercheOnSubmit() {
+
+  }
+
+  establishmentOnSubmit() {
+
+  }
 }
